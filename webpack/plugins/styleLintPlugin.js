1d3fa43c275d9c73d8ad193eca6c9a022c2fd2cc
ButNotHero <butnothero@gmail.com>
2021-02-28 12:49:09 +0700

    Micro changed

--- webpack/plugins/styleLintPlugin.js
+++ webpack/plugins/styleLintPlugin.js
@@ -10,12 +10,12 @@ const paths = require('../utils/paths');
  * A webpack plugin to lint your CSS/Sass code using stylelint.
  */
 module.exports = styleLintPlugin = new StyleLintPlugin({
-    emitErrors: true,
-    emitWarning: true,
-    context: paths.src + '',
-    extensions: ['.scss', '.sass', '.css'],
-    failOnError: !process.env.NODE_ENV === 'development',
-    failOnWarning: !process.env.NODE_ENV === 'development',
-    lintDirtyModulesOnly: process.env.NODE_ENV === 'development',
-    configFile: path.resolve(__dirname, '../../.stylelintrc.js'),
+	emitErrors: true,
+	emitWarning: true,
+	context: paths.src + '',
+	extensions: ['.scss', '.sass', '.css'],
+	failOnError: !process.env.NODE_ENV === 'development',
+	failOnWarning: !process.env.NODE_ENV === 'development',
+	lintDirtyModulesOnly: process.env.NODE_ENV === 'development',
+	configFile: path.resolve(__dirname, '../../.stylelintrc.js'),
 });
